97556478da72a59259f7775542ae110fbf7e5721
martin <martdevelopers254@gmail.com>
2020-07-26 00:46:24 +0300

    Implemented Create On Fleet Routes

---
+++ resources/views/iFleet-Core/sudo_add_route.php
@@ -0,0 +1,115 @@
+<?php
+    session_start();
+    include('config/config.php');
+    include('config/checklogin.php');
+    check_login();
+    
+    if(isset($_POST['add_route']))
+    {
+            $error = 0;
+            if (isset($_POST['route_name']) && !empty($_POST['route_name'])) {
+                $route_name=mysqli_real_escape_string($mysqli,trim($_POST['route_name']));
+            }else{
+                $error = 1;
+                $err="Route Name Cannot Be Empty";
+            }                        
+            
+            $route_code = $_POST['route_code'];
+            $route_name = $_POST['route_name'];
+            $route_description = $_POST['route_description'];
+            
+            //Insert Captured information to a database table
+            $postQuery="INSERT INTO iFleet_Route (route_code, route_name, route_description) VALUES (?,?,?) ";
+            $postStmt = $mysqli->prepare($postQuery);
+            //bind paramaters
+            $rc=$postStmt->bind_param('sss', $route_code, $route_name, $route_description);
+            $postStmt->execute();
+            //declare a varible which will be passed to alert function
+            if($postStmt)
+            {
+                $success = "Route Added" && header("refresh:1; url=sudo_add_route.php");
+            }
+            else 
+            {
+                $err = "Please Try Again Or Try Later";
+            } 
+        }
+    require_once('partials/_head.php');
+    require_once('partials/_codeGen.php');
+?>
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+<div class="wrapper">
+  <!-- Navbar -->
+    <?php require_once('partials/_nav.php');?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+    <?php require_once('partials/_sidenav.php');?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Fleet Routes Management</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="sudo_dashboard.php">Dashboard</a></li>
+              <li class="breadcrumb-item"><a href="sudo_add_route.php">Fleet Routes</a></li>
+              <li class="breadcrumb-item active">Register New Route</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+                <h3>Fill All Required Details</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                <form role="form" method='post' enctype="multipart/form-data">
+                    <div class="card-body">
+                        <div class="form-row">
+                            <div class="form-group col-md-6">
+                                <label for="exampleInputEmail1">Route Name</label>
+                                <input type="text" name="route_name"  class="form-control">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <label for="exampleInputPassword1">Route Code |  Number</label>
+                                <input type="text" class="form-control" readonly name="route_code" value="iFleet-<?php echo $beta;?>-<?php echo $alpha;?>">
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="form-group col-md-12">
+                                <label for="exampleInputEmail1">Route Description</label>
+                                <textarea type="text" name="route_description"  class="form-control" ></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer">
+                        <button type="submit" name="add_route" class="btn btn-outline-primary"><i class="fas fa-save"></i> Save</button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+      </div>
+    </section>
+  </div>
+  <?php require_once('partials/_footer.php');?>
+</div>
+<?php require_once("partials/_scripts.php");?>
+</body>
+</html>
