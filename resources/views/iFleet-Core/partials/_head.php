c13e846462da12a35edcf163cf27df94e29992bb
martin <martdevelopers254@gmail.com>
2020-07-25 06:18:08 +0300

    Loaded Swal On Head Partial

--- resources/views/iFleet-Core/partials/_head.php
+++ resources/views/iFleet-Core/partials/_head.php
@@ -20,5 +20,5 @@
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
   <!--Inject Sweet alerts-->
-  
+  <script src="assets/dist/js/swal.js"></script>
 </head>
