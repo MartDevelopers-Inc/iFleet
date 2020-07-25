7df63753305db4666ae454363d99a27dcf9b80b7
martin <martdevelopers254@gmail.com>
2020-07-25 06:49:23 +0300

    Implemented Create On HRM

---
+++ resources/views/iFleet-Core/sudo_hrm_add_staff.php
@@ -0,0 +1,61 @@
+<?php
+    session_start();
+    include('config/config.php');
+    include('config/checklogin.php');
+    check_login();
+    
+    require_once('partials/_head.php');
+?>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <!-- Navbar -->
+    <?php require_once('partials/_nav.php');?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+    <?php require_once('partials/_sidenav.php');?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Human Resource Management</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="sudo_dashboard.php">Dashboard</a></li>
+              <li class="breadcrumb-item"><a href="sudo_hrm.php">HRM</a></li>
+              <li class="breadcrumb-item active">Add New Staff</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
+            <div class="card-header">
+                <h3>Fill All Required Details</h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col -->
+      </div>
+    </section>
+  </div>
+  <?php require_once('partials/_footer.php');?>
+</div>
+<?php require_once("partials/_scripts.php");?>
+</body>
+</html>
