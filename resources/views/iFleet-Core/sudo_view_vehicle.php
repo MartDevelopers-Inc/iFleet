5c0bb9361b450fdcadcea431ebecf2d1cdeb8ebd
martin <martdevelopers254@gmail.com>
2020-07-27 11:47:11 +0300

    Fully Implemented Vehicles Module

--- resources/views/iFleet-Core/sudo_view_vehicle.php
+++ resources/views/iFleet-Core/sudo_view_vehicle.php
@@ -14,12 +14,12 @@
     <!-- Main Sidebar Container -->
         <?php 
             require_once('partials/_sidenav.php');
-            $fleet_id = $_GET['fleet_id'];
-            $ret="SELECT * FROM  iFleet_fleet WHERE fleet_id = '$fleet_id' "; 
+            $reg= $_GET['reg'];
+            $ret="SELECT * FROM  iFleet_Vehicles WHERE v_reg_no = '$reg' "; 
             $stmt= $mysqli->prepare($ret) ;
             $stmt->execute() ;//ok
             $res=$stmt->get_result();
-            while($fleet = $res->fetch_object())
+            while($vehicle = $res->fetch_object())
             {
         ?>
     <!-- /.navbar -->
@@ -30,14 +30,13 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1><?php echo $fleet->fleet_name;?> Details</h1>
+                    <h1><?php echo $vehicle->v_name;?> Details</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="sudo_dashboard.php">Dashboard</a></li>
-                        <li class="breadcrumb-item"><a href="sudo_manage_fleet.php">Fleet</a></li>
-                        <li class="breadcrumb-item"><?php echo $fleet->fleet_category_name;?></li>
-                        <li class="breadcrumb-item"><?php echo $fleet->fleet_name;?></li>
+                        <li class="breadcrumb-item"><a href="sudo_vehicles.php">Vehicles</a></li>
+                        <li class="breadcrumb-item active"><?php echo $vehicle->v_name;?></li>
                     </ol>
                 </div>
             </div>
@@ -54,26 +53,23 @@
                             <div class="card-body box-profile">
                                 <ul class="list-group list-group-unbordered mb-3">
                                     <li class="list-group-item">
-                                        <b> <span class="text-success"> Fleet Name </span></b> <a class="float-right"><?php echo $fleet->fleet_name;?></a>
+                                        <b> <span class="text-success"> Vehicle Reg Number </span></b> <a class="float-right"><?php echo $vehicle->v_reg_no;?></a>
                                     </li>
                                     <li class="list-group-item">
-                                        <b><span class="text-success"> Fleet Number </span> </b> <a class="float-right"><?php echo $fleet->fleet_code;?></a>
+                                        <b><span class="text-success"> Vehicle Name </span> </b> <a class="float-right"><?php echo $vehicle->v_name;?></a>
                                     </li>
                                     <li class="list-group-item">
-                                        <b> <span class="text-success"> Fleet Category Name </span> </b> <a class="float-right"><?php echo $fleet->fleet_category_name;?></a>
+                                        <b> <span class="text-success"> Fleet Name </span> </b> <a class="float-right"><?php echo $vehicle->v_fleet_name;?></a>
                                     </li>
                                     <li class="list-group-item">
-                                        <b> <span class="text-success"> Number Of Vehicles </span> </b> <a class="float-right"><?php echo $fleet->vehicle_numbers;?></a>
+                                        <b> <span class="text-success"> Engine Type </span> </b> <a class="float-right"><?php echo $vehicle->v_fuel;?> Driven</a>
                                     </li>
                                     <li class="list-group-item">
-                                        <b> <span class="text-success"> Staff Incharge Number </span> </b> <a class="float-right"><?php echo $fleet->fleet_staff_number;?></a>
+                                        <b> <span class="text-success"> Vehicle Status </span> </b> <a class="float-right"><?php echo $vehicle->v_status;?></a>
                                     </li>
                                     <li class="list-group-item">
-                                        <b> <span class="text-success"> Staff Incharge Name </span> </b> <a class="float-right"><?php echo $fleet->fleet_staff_name;?></a>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <b> <span class="text-success"> Account Created At  </span> </b> 
-                                        <a class="float-right"><span class="badge badge-success"><?php echo date('d / M / Y g:i', strtotime($fleet->created_at));?></span></a>
+                                        <b> <span class="text-success"> Vehicle Created At  </span> </b> 
+                                        <a class="float-right"><span class="badge badge-success"><?php echo date('d / M / Y g:i', strtotime($vehicle->created_at));?></span></a>
                                     </li>
                                 </ul>
                             </div>
@@ -83,12 +79,12 @@
                     <div class="col-md-7">
                         <div class="card card-primary">
                             <div class="card-header">
-                                <h3 class="card-title"><?php echo $fleet->fleet_name;?> Description</h3>
+                                <h3 class="card-title"><?php echo $vehicle->v_name;?> Description</h3>
                             </div>
                             <!-- /.card-header -->
                             <div class="card-body">
                                 <p class="text-muted">
-                                    <?php echo $fleet->fleet_desc;?>
+                                    <?php echo $vehicle->v_desc;?>
                                 </p>
                             </div>
                         </div>
